a5161437ac48b795aafd2319542e36d7c4bd2bfb
veereshgunadal <gunadalveeresh@gmail.com>
2024-07-23 05:53:55 -0700

    added if condition

--- sorting.py
+++ sorting.py
@@ -1,3 +1,7 @@
 def bubble(l):
     for i in range(len(l)-1, 0 , -1):
         for j in range(i):
+            if l[j] > l[j+1]:
+                temp = l[j]
+                l[j] = l[j+1]
+                l[j+1] = temp
